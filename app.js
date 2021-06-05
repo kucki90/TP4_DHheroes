78aea629eb0a772cd5135e86c7b79dbe6a670db2
kucki90 <c.kuckiewicz@hotmail.com>
2021-06-05 19:38:19 -0300

    app ordenado

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 3030;
 const path= require('path');
 const views = path.join(__dirname,'views');
 
+app.use(express.static('public'))
+
 app.get('/', (req,res) => res.sendFile(path.join(views, 'index.html')))
 app.get('/bella.html',(req,res) => res.sendFile(path.join(views, 'bella.html')))
 app.get('/jasmine.html',(req,res) => res.sendFile(path.join(views, 'jasmine.html')))
@@ -12,8 +14,5 @@ app.get('/mulan.html',(req,res) => res.sendFile(path.join(views, 'mulan.html')))
 app.get('/pocahontas.html',(req,res) => res.sendFile(path.join(views, 'pocahontas.html')))
 app.get('/tiana.html',(req,res) => res.sendFile(path.join(views, 'tiana.html')))
 app.get('/vaiana.html',(req,res) => res.sendFile(path.join(views, 'vaiana.html')))
-
-
-app.use(express.static('public'))
 
 app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port))
