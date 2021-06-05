b2bd46f909976bf3d1e1f1910a9c85ec9d335689
kucki90 <c.kuckiewicz@hotmail.com>
2021-06-05 00:34:18 -0300

    terminado?

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const path= require('path');
 const views = path.join(__dirname,'views');
 
 app.get('/', (req,res) => res.sendFile(path.join(views, 'index.html')))
-app.get('/bella',(req,res) => res.sendFile(path.join(views, 'bella.html')))
-app.get('/jasmine',(req,res) => res.sendFile(path.join(views, 'jasmine.html')))
-app.get('/merida',(req,res) => res.sendFile(path.join(views, 'merida.html')))
-app.get('/mulan',(req,res) => res.sendFile(path.join(views, 'mulan.html')))
-app.get('/pocahontas',(req,res) => res.sendFile(path.join(views, 'pocahontas.html')))
-app.get('/tiana',(req,res) => res.sendFile(path.join(views, 'tiana.html')))
-app.get('/vaiana',(req,res) => res.sendFile(path.join(views, 'vaiana.html')))
+app.get('/bella.html',(req,res) => res.sendFile(path.join(views, 'bella.html')))
+app.get('/jasmine.html',(req,res) => res.sendFile(path.join(views, 'jasmine.html')))
+app.get('/merida.html',(req,res) => res.sendFile(path.join(views, 'merida.html')))
+app.get('/mulan.html',(req,res) => res.sendFile(path.join(views, 'mulan.html')))
+app.get('/pocahontas.html',(req,res) => res.sendFile(path.join(views, 'pocahontas.html')))
+app.get('/tiana.html',(req,res) => res.sendFile(path.join(views, 'tiana.html')))
+app.get('/vaiana.html',(req,res) => res.sendFile(path.join(views, 'vaiana.html')))
 
 
 app.use(express.static('public'))
